4c9e121d0388d85c6ae2059371d7d487c1744d23
miho-takahashi <bp16057@shibaura-it.ac.jp>
2019-03-25 17:22:29 +0900

    changed

--- time_card2.py
+++ time_card2.py
@@ -1,5 +1,6 @@
-from bluetooth.ble import BeaconService
+from bluetooth.ble import BeaconService:
 import MySQLdb
+import datatime
 
 class Beacon(object):
     
@@ -18,11 +19,26 @@ class Beacon(object):
                         MINOR=self._minor, POWER=self._power, RSSI=self._rssi)
         return ret　　　#この関数何言ってるんすか?たぶんretの中身がBeacon address:ここに今の白いとこが来るわけよ
 
+def scan_beacon(leave_user):
+    service = BeaconService()
+    devices = service.scan(2)
+    for address, data in list(devices.items()):
+        b = Beacon(data, address)
+        print(b)
+        for i in leave_user:
+            sql_str = "SELECT uuid FROM Employee WHERE Name = "+i+";"
+            cur.execute(sql_str)
+            tmp=cur.fetchall[0][0]
+          if b._uuid==tmp:
+            sql_str = "INSERT INTO time_card(Name,Time,State) VALUES("+i+","+datatime.datatime.now()+","+"enter);"
+            cur.execute(sql_str)
+    print("Done.")
+
+
 if __name__ == "__main__":
   db = MySQLdb.connect(host="localhost",user="Takasan",passwd="9230",db="time_card"
   user_name = []
   leave_user = []
-  enter_user = []
   cur = db.cursor() #カーソル　これはおまじないな気がする俺はわからん
   sql_str = "SELECT Name FROM Employee;" #なにこれ?EMployeeのテーブルから名前を引っ張ってきてる
   if cur.execute(sql_str):
@@ -30,26 +46,12 @@ if __name__ == "__main__":
   while(1):
     sleep(1)
     for n in user_name:
-      cur.execute("SELECT state FROM time_card WHERE timestamp = (SELECT max(timestamp) FROM time_card and Name ="+n+";")
+      cur.execute("SELECT state FROM time_card WHERE Name ="+n+";")
       tmp=cur.fetchall[0][0]
-      if tmp=="leave":
-        leave_user.append(tmp)
-      else:
-        enter_user.append(tmp)
-    service = BeaconService()
-    devices = service.scan(2)＃この辺の処理書き直す？　めちゃ汚いことになてる ちょっと待って、ねるのことでいっぱいで見逃したーー
-    for address, data in list(devices.items()):
-        flag = 0
-        b = Beacon(data, address)
-        print(b)
-        for i in leave_user:
-          if b._uuid==i:
-            sql_str = "INSERT INTO time_card(Name,Time,State) VALUES(TEXT,TIME_STAMP,TEXT);"
-        for i in enter_user:
-          if b._uuid = i:
-            flag= 1
-        
+      if tmp[0][0][len(tmp[0][0])-1]=="leave":
+        leave_user.append(n)
+    scan_beacon(leave_user)
 
-    print("Done.")
-  
+
+
   
